6bb28c8ce67c52f6396af362e664c303beaaeaf8
DanielSbirna <costel.daniel.sbirna@gmail.com>
2026-07-07 18:31:44 +0300

    Added the new suggestion variable to match the rest of the codebase

--- app/src/main/java/com/example/macrotracker/data/MacroEstimate.java
+++ app/src/main/java/com/example/macrotracker/data/MacroEstimate.java
@@ -6,16 +6,19 @@ public class MacroEstimate {
     private final BigDecimal protein;
     private final BigDecimal carbs;
     private final BigDecimal fats;
+    private final String suggestion;
 
-    public MacroEstimate(BigDecimal calories, BigDecimal protein, BigDecimal carbs, BigDecimal fats) {
+    public MacroEstimate(BigDecimal calories, BigDecimal protein, BigDecimal carbs, BigDecimal fats, String suggestion) {
         this.calories = calories;
         this.protein = protein;
         this.carbs = carbs;
         this.fats = fats;
+        this.suggestion = suggestion;
     }
 
     public BigDecimal getCalories() {return calories;}
     public BigDecimal getProtein() {return protein;}
     public BigDecimal getCarbs() {return carbs;}
     public BigDecimal getFats() {return fats;}
+    public String getSuggestion() {return suggestion;}
 }
