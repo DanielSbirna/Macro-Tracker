8aadfec8ab8881e69ffc8e0c45eb74eaf77e64ec
DanielSbirna <costel.daniel.sbirna@gmail.com>
2026-07-07 14:48:47 +0300

    Created RecalibrationCallback, interface with 3 outcomes suggestion available, no suggestion and error

--- app/src/main/java/com/example/macrotracker/util/RecalibrationCallback.java
+++ app/src/main/java/com/example/macrotracker/util/RecalibrationCallback.java
@@ -1,4 +1,7 @@
 package com.example.macrotracker.util;
 
 public interface RecalibrationCallback {
+    void onSuggestionAvailable(RecalibrationResult result);
+    void onNoSuggestion(String reason); //not due yet or insufficient data
+    void onError(Exception e);
 }
