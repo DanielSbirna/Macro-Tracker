03b52451ea308eec8d5eabf2c8b35322db900987
DanielSbirna <costel.daniel.sbirna@gmail.com>
2026-07-20 00:32:51 +0300

    add title field to MacroEstimate model data structure

--- app/src/main/java/com/example/macrotracker/models/MacroEstimate.java
+++ app/src/main/java/com/example/macrotracker/models/MacroEstimate.java
@@ -2,13 +2,15 @@ package com.example.macrotracker.models;
 
 import java.math.BigDecimal;
 public class MacroEstimate {
+    private final String title;
     private final BigDecimal calories;
     private final BigDecimal protein;
     private final BigDecimal carbs;
     private final BigDecimal fats;
     private final String suggestion;
 
-    public MacroEstimate(BigDecimal calories, BigDecimal protein, BigDecimal carbs, BigDecimal fats, String suggestion) {
+    public MacroEstimate(String title, BigDecimal calories, BigDecimal protein, BigDecimal carbs, BigDecimal fats, String suggestion) {
+        this.title = title;
         this.calories = calories;
         this.protein = protein;
         this.carbs = carbs;
@@ -16,6 +18,7 @@ public class MacroEstimate {
         this.suggestion = suggestion;
     }
 
+    public String getTitle() {return title;}
     public BigDecimal getCalories() {return calories;}
     public BigDecimal getProtein() {return protein;}
     public BigDecimal getCarbs() {return carbs;}
